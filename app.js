e26ac98ea5d79dc840c99e7b395fc19e14a04663
Michael-Pellizzato <pellizzato.michael97@gmail.com>
2025-03-18 23:32:49 +0100

    aggiunta cors

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
 import moviesRouter from "./routes/moviesRouter.js";
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 app.use(express.json());
 app.use("/movies", moviesRouter);
